98cd4dd1ed76441c76622222e6dc5bff01916832
Atul Rathod <atulrathodc@gmail.com>
2025-01-08 21:48:39 +0530

    tyuy

--- src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/SpringDataJpaTutorialApplication1.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/SpringDataJpaTutorialApplication1.java
@@ -1,10 +1,8 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.ApplicationContext;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +17,8 @@ class SpringDataJpaTutorialApplication1 {
 	public int multi(int a, int b) {
 		return a + b;
 	}
-	public static void main(String[] args) throws JsonProcessingException {
-		ApplicationContext context = SpringApplication.run(SpringDataJpaTutorialApplication1.class, args);
+	public static void main(String[] args){
+	SpringApplication.run(SpringDataJpaTutorialApplication1.class, args);
 
 	}
 }
