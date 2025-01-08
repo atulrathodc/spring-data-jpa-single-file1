af9dda6784ee4a0d2dd02eb9b17accd5a9372899
Atul Rathod <atulrathodc@gmail.com>
2025-01-08 21:42:46 +0530

    tyuy

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/SpringDataJpasTutorialApplicationTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/SpringDataJpasTutorialApplicationTest.java
@@ -1,28 +1,28 @@
-//package com.dailycodebuffer.spring.data.jpa.tutorial;
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class SpringDataJpasTutorialApplicationTest {
-//    @Test
-//    void contextLoads() {
-//        // This test ensures the Spring application context loads successfully
-//    }
-//    @Test
-//    void testMainMethod() throws JsonProcessingException {
-//        // Call the main method to ensure it runs without issues
-//        SpringDataJpaTutorialApplication1.main(new String[] {});
-//    }
-//    @Test
-//    public void testAdd() {
-//        SpringDataJpaTutorialApplication1 app = new SpringDataJpaTutorialApplication1();
-//        assert(app.add(1, 2) == 3);
-//    }
-//    @Test
-//    public void multi() {
-//        SpringDataJpaTutorialApplication1 app = new SpringDataJpaTutorialApplication1();
-//        assert(app.multi(1, 2) == 3);
-//    }
-//
-//}
+package com.dailycodebuffer.spring.data.jpa.tutorial;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringDataJpasTutorialApplicationTest {
+    @Test
+    void contextLoads() {
+        // This test ensures the Spring application context loads successfully
+    }
+    @Test
+    void testMainMethod() throws JsonProcessingException {
+        // Call the main method to ensure it runs without issues
+        SpringDataJpaTutorialApplication1.main(new String[] {});
+    }
+    @Test
+    public void testAdd() {
+        SpringDataJpaTutorialApplication1 app = new SpringDataJpaTutorialApplication1();
+        assert(app.add(1, 2) == 3);
+    }
+    @Test
+    public void multi() {
+        SpringDataJpaTutorialApplication1 app = new SpringDataJpaTutorialApplication1();
+        assert(app.multi(1, 2) == 3);
+    }
+
+}
